57fa2d3b56f483fb3b3f1863538ff1261b222ad0
Cripa Varghese <cripavarghese@Cripas-MacBook-Air.local>
2023-05-07 01:58:47 +0530

    feat added: Filter and Edit

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const formInput = document.getElementById("form-input");
 const listProducts = document.getElementById("list-products");
 const clearButton = document.getElementById("clear-button");
 const filter = document.getElementById("filter");
+const formButton = document.querySelector("button");
+let isEditMode = false;
 
 const displayItemsFrmLocalStorg = () => {
   let getProductsFromLocalStr = getProductsFromLocalStorage();
   getProductsFromLocalStr.forEach((product) => {
     addProductToDom(product);
-    checkUiFilterClearButtn();
+    checkUI();
   });
 };
 const addProductSubmit = (e) => {
@@ -19,11 +21,24 @@ const addProductSubmit = (e) => {
     alert("please fill the field with a product");
     return;
   }
+
+  if (isEditMode) {
+    const productToEdit = listProducts.querySelector(".edit-mode");
+    removeProductFromLocalStrg(productToEdit.textContent);
+    productToEdit.classList.remove("edit-mode");
+    productToEdit.remove();
+    isEditMode = false;
+  } else {
+    if (checkProductExists(newProduct)) {
+      alert("This product already exists");
+      return;
+    }
+  }
   addProductToDom(newProduct);
 
   addProductToLocalStorage(newProduct);
 
-  checkUiFilterClearButtn();
+  checkUI();
   formInput.value = "";
 };
 
@@ -35,8 +50,30 @@ const addProductToDom = (product) => {
   li.className = "products";
 
   const deleteButton = createDeleteButton();
+  const editButton = createEditButton();
   li.appendChild(deleteButton);
+  li.appendChild(editButton);
   listProducts.appendChild(li);
+};
+
+const createDeleteButton = () => {
+  const deleteButton = document.createElement("button");
+  const icon = createIcon("fa-solid fa-trash");
+  deleteButton.appendChild(icon);
+  return deleteButton;
+};
+
+const createEditButton = () => {
+  const editButton = document.createElement("button");
+  const icon = createIcon("fa-solid fa-pencil");
+  editButton.appendChild(icon);
+  return editButton;
+};
+
+const createIcon = (className) => {
+  const i = document.createElement("i");
+  i.className = className;
+  return i;
 };
 
 const addProductToLocalStorage = (product) => {
@@ -56,31 +93,37 @@ const getProductsFromLocalStorage = () => {
   return itemsFromLocalStorage;
 };
 
-const createDeleteButton = () => {
-  const button = document.createElement("button");
-  const icon = createDeleteIcon("fa-solid fa-trash");
-  button.appendChild(icon);
-  return button;
-};
-
-const createDeleteIcon = (className) => {
-  const i = document.createElement("i");
-  i.className = className;
-  return i;
-};
-
 const onClickProduct = (e) => {
-  if (e.target.parentElement.parentElement.classList.contains("products")) {
-    console.log(e.target.parentElement.parentElement);
+  if (e.target.classList.contains("fa-pencil")) {
+    editProduct(e.target.parentElement.parentElement);
+  } else if (e.target.classList.contains("fa-trash")) {
     removeProduct(e.target.parentElement.parentElement);
   }
 };
 
+const checkProductExists = (product) => {
+  const itemsFrmlocalStorage = getProductsFromLocalStorage();
+  return itemsFrmlocalStorage.includes(product);
+};
+
+const editProduct = (product) => {
+  isEditMode = true;
+  const productsLi = listProducts.querySelectorAll("li");
+  productsLi.forEach((prod) => {
+    prod.classList.remove("edit-mode");
+  });
+  product.classList.add("edit-mode");
+  formButton.textContent = "Update here";
+  formButton.style.backgroundColor = "green";
+  formButton.style.color = "white";
+  formInput.value = product.textContent;
+};
+
 const removeProduct = (product) => {
-  if (confirm("Are you sure to delete the product")) {
+  if (confirm("Are you sure to delete this product?")) {
     product.remove();
     removeProductFromLocalStrg(product.textContent);
-    checkUiFilterClearButtn();
+    checkUI();
   }
 };
 
@@ -95,15 +138,15 @@ const clearProducts = () => {
   while (listProducts.firstChild) {
     listProducts.removeChild(listProducts.firstChild);
   }
-  checkUiFilterClearButtn();
+  checkUI();
 };
 
 const filterItem = (e) => {
   const productsLi = listProducts.querySelectorAll("li");
-  const text = e.target.value;
+  const text = e.target.value.toLowerCase();
 
   productsLi.forEach((product) => {
-    const productName = product.firstChild.textContent;
+    const productName = product.firstChild.textContent.toLowerCase();
 
     if (productName.indexOf(text) != -1) {
       product.style.display = "block";
@@ -113,7 +156,8 @@ const filterItem = (e) => {
   });
 };
 
-const checkUiFilterClearButtn = () => {
+const checkUI = () => {
+  formInput.value = "";
   const productsLi = listProducts.querySelectorAll("li");
   if (productsLi.length === 0) {
     clearButton.style.display = "none";
@@ -122,6 +166,10 @@ const checkUiFilterClearButtn = () => {
     clearButton.style.display = "block";
     filter.style.display = "block";
   }
+  formButton.innerHTML = "Add item";
+  formButton.style.backgroundColor = "white";
+  formButton.style.color = "black";
+  isEditMode = false;
 };
 
 form.addEventListener("submit", addProductSubmit);
@@ -130,4 +178,4 @@ clearButton.addEventListener("click", clearProducts);
 filter.addEventListener("input", filterItem);
 document.addEventListener("DOMContentLoaded", displayItemsFrmLocalStorg);
 
-checkUiFilterClearButtn();
+checkUI();
